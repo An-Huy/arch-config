01b746e748e7d7347b4d244a3abe4d9e9d98d71e
A.Huy <90856473+An-Huy@users.noreply.github.com>
2022-11-12 11:16:28 +0700

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,23 +7,7 @@ git clone https://aur.archlinux.org/yay.git $HOME/yay/
 cd $HOME/yay/ && makepkg -si		
 
 # bspwm & polybar
-sudo pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd rofi terminator picom 
-sudo pacman -S --noconfirm arandr feh dunst redshift flameshot stalonetray neofetch jgmenu 
-yay -S --noconfirm polybar pacman-contrib pulseaudio ttf-font-awsome siji-git alsa-utils		
-cp -r $HOME/arch-config/wallpaper/ $HOME/
-
-if [ -f $HOME/.xinitrc ]; then
-    echo "File already exist, change file to .bak"
-    sudo mv $HOME/.xinitrc $HOME/.xinitrc.bak && sudo cp $HOME/arch-config/dotfiles/.xinitrc $HOME/.xinitrc
-else
-    sudo cp $HOME/arch-config/dotfiles/.xinitrc $HOME/.xinitrc
-fi
-
-if [ -d $HOME/.config/ ]; then
-	cp -r $HOME/arch-config/.config/* $HOME/.config/
-else
-    mkdir -p $HOME/.config/ && cp -r $HOME/arch-config/.config/* $HOME/.config/
-fi	
+bash wm.sh
 
 # AstroNvim	
 sudo pacman -S --noconfirm neovim nodejs npm	
@@ -34,7 +18,6 @@ sudo pacman -S --noconfirm libxext libxres ffmpegthumbnailer imagemagick poppler
 yay -S --noconfirm epub-thumbnailer-git lf	
 	
 # Zsh
-sudo pacman -S --noconfirm zsh
 bash $HOME/arch-config/scripts/zsh.sh
 
 sudo reboot now		
